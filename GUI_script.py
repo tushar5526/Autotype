92acc67ea0abae7b89ce6a03838fb6f5f6a5c1d2
Kanishk Pachauri <krishna03012003@icloud.com>
2022-06-29 20:51:21 +0530

    Added time Delay

--- GUI_script.py
+++ GUI_script.py
@@ -1,5 +1,6 @@
 # Imports
 import sys
+
 if sys.version_info[0] == 3:
     import tkinter as tk
     from tkinter import filedialog
@@ -8,21 +9,6 @@ else:
     from TKinter import filedialog
 from Simulator.simulate_keyboard import Type
 from PIL import ImageTk, Image
-
-
-
-# Function for Opening File
-def open_file():
-    filePath = filedialog.askopenfile()
-    return filePath.name
-
-# Function for Writing File
-def writer():
-    Type(path=open_file(),delay=3)
-    done_task = tk.Label(text="Done Writting Script")
-    done_task.pack()
-
-
 
 # GUI using TKinter
 
@@ -38,16 +24,46 @@ frame = tk.Frame(root, width=600, height=400)
 frame.pack()
 frame.place(anchor='center', relx=0.5, rely=0.5)
 body_image = ImageTk.PhotoImage(Image.open('gui_assets/type.jpg'))
-label = tk.Label(frame, image = body_image)
+label = tk.Label(frame, image=body_image)
 label.pack()
 
-
 # Labeling the window
-Title = tk.Label(root, text="AutoType 🖊", fg="#8FBDD3", font=('',62))
+Title = tk.Label(root, text="AutoType 🖊", fg="#8FBDD3", font=('', 62))
 Title.pack()
 
+# Input Box for Time Delay
+entry1 = tk.Entry(root, borderwidth=5)
+entry1.pack()
+entry1.insert(0, 3)
+
+
+# Logic
+def add_time_delay():
+    mylable_time = tk.Label(root, text=f"Time Delay of {entry1.get()} seconds added")
+    mylable_time.pack()
+
+
+# Function for Opening File
+def open_file():
+    filePath = filedialog.askopenfile()
+    return filePath.name
+
+
+# Function for Writing File
+def writer():
+    Type(path=open_file(), delay=int(entry1.get()))
+    done_task = tk.Label(text="Done Writing Script")
+    done_task.pack()
+
+
 # Button widget instance
-btn1 = tk.Button(root, text="Write File", activebackground='#345',activeforeground='white', padx=5, pady=5, command=writer )
+btn0 = tk.Button(root, text="Add time Delay in Seconds", activebackground='#345', activeforeground='white', padx=5,
+                 pady=5, command=add_time_delay)
+btn0.pack()
+
+# Button widget instance
+btn1 = tk.Button(root, text="Write File", activebackground='#345', activeforeground='white', padx=5, pady=5,
+                 command=writer)
 btn1.pack()
 
 root.mainloop()
