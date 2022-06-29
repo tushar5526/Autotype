caa0c553036824e4eea7a7d566cd49ca79fde4b5
Kanishk Pachauri <krishna03012003@icloud.com>
2022-06-29 20:58:35 +0530

    Improve code to be more clean

--- GUI_script.py
+++ GUI_script.py
@@ -10,36 +10,37 @@ else:
 from Simulator.simulate_keyboard import Type
 from PIL import ImageTk, Image
 
-# GUI using TKinter
+# GUI using Tkinter
 
 # Root Widget
-root = tk.Tk()
-root.title("KeyBoard Simulator")
+root_widget = tk.Tk()
+root_widget.title("KeyBoard Simulator")
 
 # Width and Height
-root.geometry('780x480')
+root_widget.geometry('780x480')
 
 # Image
-frame = tk.Frame(root, width=600, height=400)
+frame = tk.Frame(root_widget, width=600, height=400)
 frame.pack()
 frame.place(anchor='center', relx=0.5, rely=0.5)
 body_image = ImageTk.PhotoImage(Image.open('gui_assets/type.jpg'))
-label = tk.Label(frame, image=body_image)
-label.pack()
+label_for_body_image = tk.Label(frame, image=body_image)
+label_for_body_image.pack()
 
-# Labeling the window
-Title = tk.Label(root, text="AutoType 🖊", fg="#8FBDD3", font=('', 62))
-Title.pack()
+# Labeling the window title
+root_window_title = tk.Label(root_widget, text="AutoType 🖊", fg="#8FBDD3", font=('', 62))
+root_window_title.pack()
 
 # Input Box for Time Delay
-entry1 = tk.Entry(root, borderwidth=5)
-entry1.pack()
-entry1.insert(0, 3)
+entry_for_time_delay = tk.Entry(root_widget, borderwidth=5)
+entry_for_time_delay.pack()
+entry_for_time_delay.insert(0, 3)
 
 
 # Logic
+# Function for Addition of Time Delay
 def add_time_delay():
-    mylable_time = tk.Label(root, text=f"Time Delay of {entry1.get()} seconds added")
+    mylable_time = tk.Label(root_widget, text=f"Time Delay of {entry_for_time_delay.get()} seconds added")
     mylable_time.pack()
 
 
@@ -49,21 +50,21 @@ def open_file():
     return filePath.name
 
 
-# Function for Writing File
-def writer():
-    Type(path=open_file(), delay=int(entry1.get()))
+# Function for Writing File through keyboard
+def file_writer():
+    Type(path=open_file(), delay=int(entry_for_time_delay.get()))
     done_task = tk.Label(text="Done Writing Script")
     done_task.pack()
 
 
 # Button widget instance
-btn0 = tk.Button(root, text="Add time Delay in Seconds", activebackground='#345', activeforeground='white', padx=5,
-                 pady=5, command=add_time_delay)
-btn0.pack()
+button_for_add_time_delay = tk.Button(root_widget, text="Add time Delay in Seconds", activebackground='#345', activeforeground='white', padx=5,
+                                      pady=5, command=add_time_delay)
+button_for_add_time_delay.pack()
 
 # Button widget instance
-btn1 = tk.Button(root, text="Write File", activebackground='#345', activeforeground='white', padx=5, pady=5,
-                 command=writer)
-btn1.pack()
+button_for_writing_file = tk.Button(root_widget, text="Write File", activebackground='#345', activeforeground='white', padx=5, pady=5,
+                                    command=file_writer)
+button_for_writing_file.pack()
 
-root.mainloop()
+root_widget.mainloop()
