de98cdef7cc6b8000c6a23d554160ef37be9055d
Mr-Sunglasses <Mr-Sunglasses@users.noreply.github.com>
2022-09-11 11:02:40 +0000

    :art: Python code fromated with psf/black

--- GUI_script.py
+++ GUI_script.py
@@ -17,18 +17,20 @@ root_widget = tk.Tk()
 root_widget.title("KeyBoard Simulator")
 
 # Width and Height
-root_widget.geometry('780x480')
+root_widget.geometry("780x480")
 
 # Image
 frame = tk.Frame(root_widget, width=600, height=400)
 frame.pack()
-frame.place(anchor='center', relx=0.5, rely=0.5)
-body_image = ImageTk.PhotoImage(Image.open('gui_assets/type.jpg'))
+frame.place(anchor="center", relx=0.5, rely=0.5)
+body_image = ImageTk.PhotoImage(Image.open("gui_assets/type.jpg"))
 label_for_body_image = tk.Label(frame, image=body_image)
 label_for_body_image.pack()
 
 # Labeling the window title
-root_window_title = tk.Label(root_widget, text="AutoType 🖊", fg="#8FBDD3", font=('', 62))
+root_window_title = tk.Label(
+    root_widget, text="AutoType 🖊", fg="#8FBDD3", font=("", 62)
+)
 root_window_title.pack()
 
 # Input Box for Time Delay
@@ -40,7 +42,9 @@ entry_for_time_delay.insert(0, 3)
 # Logic
 # Function for Addition of Time Delay
 def add_time_delay():
-    mylable_time = tk.Label(root_widget, text=f"Time Delay of {entry_for_time_delay.get()} seconds added")
+    mylable_time = tk.Label(
+        root_widget, text=f"Time Delay of {entry_for_time_delay.get()} seconds added"
+    )
     mylable_time.pack()
 
 
@@ -58,13 +62,27 @@ def file_writer():
 
 
 # Button widget instance
-button_for_add_time_delay = tk.Button(root_widget, text="Add time Delay in Seconds", activebackground='#345', activeforeground='white', padx=5,
-                                      pady=5, command=add_time_delay)
+button_for_add_time_delay = tk.Button(
+    root_widget,
+    text="Add time Delay in Seconds",
+    activebackground="#345",
+    activeforeground="white",
+    padx=5,
+    pady=5,
+    command=add_time_delay,
+)
 button_for_add_time_delay.pack()
 
 # Button widget instance
-button_for_writing_file = tk.Button(root_widget, text="Write File", activebackground='#345', activeforeground='white', padx=5, pady=5,
-                                    command=file_writer)
+button_for_writing_file = tk.Button(
+    root_widget,
+    text="Write File",
+    activebackground="#345",
+    activeforeground="white",
+    padx=5,
+    pady=5,
+    command=file_writer,
+)
 button_for_writing_file.pack()
 
 root_widget.mainloop()
